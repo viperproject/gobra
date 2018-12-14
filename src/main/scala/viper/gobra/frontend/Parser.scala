42e2f59c9b9952a70ea7bdd751c0f97b421b5c5f
Felix A. Wolf <felix.wolf@inf.ethz.ch>
2018-12-14 12:26:09 +0100

    lunch commit

--- src/main/scala/viper/gobra/frontend/Parser.scala
+++ src/main/scala/viper/gobra/frontend/Parser.scala
@@ -10,7 +10,7 @@ import java.io.File
 
 import org.bitbucket.inkytonik.kiama.parsing.Parsers
 import org.bitbucket.inkytonik.kiama.util.Positions
-import viper.gobra.ast.parser.PProgram
+import viper.gobra.ast.parser._
 import viper.gobra.reporting.VerifierError
 
 object Parser {
@@ -21,7 +21,48 @@ object Parser {
 
   private class SyntaxAnalyzer(positions: Positions) extends Parsers(positions) {
 
+    val reservedWords: Set[String] = Set(
+      "break", "default", "func", "interface", "select",
+      "case", "defer", "go", "map", "struct",
+      "chan", "else", "goto", "package", "switch",
+      "const", "fallthrough", "if", "range", "type",
+      "continue", "for", "import", "return", "var"
+    )
+
+    def isReservedWord(word: String): Boolean = reservedWords contains word
+
+    /**
+      * Types
+      */
+
+    lazy val namedType: Parser[PNamedType] = ???
+
+
+    /**
+      * Identifiers
+      */
+
+    lazy val idnUnknown: Parser[PIdnUnknown] =
+      identifier ^^ PIdnUnknown
+
+    lazy val idnDef: Parser[PIdnDef] =
+      identifier ^^ PIdnDef
+
+    lazy val idnUse: Parser[PIdnUse] =
+      idnUnqualifiedUse
+
+    lazy val idnUnqualifiedUse: Parser[PIdnUnqualifiedUse] =
+      identifier ^^ PIdnUnqualifiedUse
+
+    lazy val identifier: Parser[String] =
+      "[a-zA-Z_][a-zA-Z0-9_]*".r into (s => {
+        if (isReservedWord(s))
+          failure(s"""keyword "$s" found where identifier expected""")
+        else
+          success(s)
+      })
   }
+
 }
 
 

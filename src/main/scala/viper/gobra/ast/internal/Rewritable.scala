79fc3df184516fffca635507586284ea04528453
felix wolf <felix.wolf@inf.ethz.ch>
2019-09-24 21:03:53 +0200

    adapted to changes in Silver

--- src/main/scala/viper/gobra/ast/internal/Rewritable.scala
+++ src/main/scala/viper/gobra/ast/internal/Rewritable.scala
@@ -1,8 +1,30 @@
 package viper.gobra.ast.internal
 
-import viper.silver.{ast => vpr}
-
 /**
   * Trait Rewritable defines required methods for rewriting
   */
-trait Rewritable extends Product with vpr.utility.rewriter.Rewritable
+trait Rewritable extends Product {
+
+  /**
+    * Method that accesses all children of a node.
+    * We allow 3 different types of children: Rewritable, Seq[Rewritable] and Option[Rewritable]
+    * The supertype of all 3 is AnyRef
+    * @return Sequence of children
+    */
+  def getChildren: Seq[AnyRef] = {
+    ((0 until productArity) map { x: Int => productElement(x) }) collect {
+      case s: Seq[Rewritable @unchecked] => s
+      case o: Option[Rewritable @unchecked] => o
+      case i: Rewritable => i
+    }
+  }
+
+  /**
+    * Duplicate children. Children list must be in the same order as in getChildren
+    * @see [[Rewritable.getChildren()]] to see why we use type AnyRef for children
+    *
+    * @param children New children for this node
+    * @return Duplicated node
+    */
+  def duplicate(children: Seq[AnyRef]): Rewritable
+}

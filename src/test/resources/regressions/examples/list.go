390c48e7801dcd9b59325957be56c0f6e0813955
Felix A. Wolf <felix.wolf@inf.ethz.ch>
2019-12-04 14:07:12 +0100

    added more hackathon examples to the tests

--- src/test/resources/regressions/examples/list.go
+++ src/test/resources/regressions/examples/list.go
@@ -1,10 +1,40 @@
 
-package pkg;
+package list;
 
-type cell struct{
-		val int;
+type node struct {
+  value int;
+  next *node;
 };
 
-func client() {
-    x := cell{42};
+pred list(ptr *node) {
+  ptr != nil ==> acc(ptr.value) && acc(ptr.next) && list(ptr.next)
 };
+
+
+
+requires list(ptr);
+requires list(rev);
+ensures list(res);
+func tail_reverse (ptr, rev *node) (res *node) {
+  unfold list(ptr);
+  if(ptr == nil) {
+    return rev;
+  } else {
+    tmp := ptr;
+    ptr = ptr.next;
+    tmp.next = rev;
+    fold list(tmp);
+    return tail_reverse(ptr,tmp);
+  };
+};
+
+requires list(ptr);
+ensures list (res);
+func reverse (ptr *node) (res *node) {
+  fold list(nil);
+  return tail_reverse(ptr,nil);
+};
+
+
+
+

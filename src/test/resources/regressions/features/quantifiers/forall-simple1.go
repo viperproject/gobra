1ee29250a0748cd85735164085c5f41793e3e3b6
Wytse Oortwijn <wytseoortwijn@gmail.com>
2020-03-29 13:01:37 +0200

    added regression tests

--- src/test/resources/regressions/features/quantifiers/forall-simple1.go
+++ src/test/resources/regressions/features/quantifiers/forall-simple1.go
@@ -1,4 +1,16 @@
 package pkg;
 
-requires forall n int :: true;
-func test1 () { };
+requires forall m int :: true
+func test1 () { }
+
+requires forall x int, y int :: x < y
+requires forall x int, y int :: { x < y } x < y
+func test2 () { }
+
+func test3() {
+  assert forall n int :: true
+  assert forall n int :: { n } true
+}
+
+requires forall x int, y int :: { x, y } x < y
+func test4 () { }

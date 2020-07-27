a5058a72b250776d5ad6330f0db9ee09da26f0d7
Silas Walker <walkers@student.ethz.ch>
2020-07-27 17:24:44 +0200

    Added overall message for silicon backend

--- src/main/scala/viper/gobra/backend/Silicon.scala
+++ src/main/scala/viper/gobra/backend/Silicon.scala
@@ -2,11 +2,11 @@ package viper.gobra.backend
 
 import viper.silicon
 import viper.silver
-import viper.silver.reporter.Reporter
+import viper.silver.reporter._
 
 
 import viper.silver.ast.Program
-import viper.silver.verifier.VerificationResult
+import viper.silver.verifier.{ VerificationResult, Success, Failure }
 
 import scala.concurrent.{ExecutionContextExecutor, Future, ExecutionContext}
 import viper.server.ViperBackendConfig
@@ -19,9 +19,17 @@ class Silicon(commandLineArguments: Seq[String]) extends ViperVerifier {
     Future {
       val backend: silicon.Silicon = silicon.Silicon.fromPartialCommandLineArguments(commandLineArguments, reporter)
       
+      val startTime = System.currentTimeMillis()
       backend.start()
       val result = backend.verify(program)
       backend.stop()
+
+      result match {
+        case Success =>
+          reporter report OverallSuccessMessage(backend.name, System.currentTimeMillis() - startTime)
+        case f@Failure(_) =>
+          reporter report OverallFailureMessage(backend.name, System.currentTimeMillis() - startTime, f)
+      }
 
       result
     }

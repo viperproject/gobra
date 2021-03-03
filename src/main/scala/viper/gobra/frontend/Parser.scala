b150d082d3ef38609de1a78042cc9a95439ec9a9
João Pereira <joaopereira.19@gmail.com>
2021-03-03 20:19:41 +0100

    Fix issue

--- src/main/scala/viper/gobra/frontend/Parser.scala
+++ src/main/scala/viper/gobra/frontend/Parser.scala
@@ -222,7 +222,7 @@ object Parser {
       val r = s"($finalTokenRequiringSemicolon)((?:$ignoreComments|$ignoreWhitespace)*)$$".r
       // group(1) contains the finalTokenRequiringSemicolon after which a semicolon should be inserted
       // group(2) contains the line's remainder after finalTokenRequiringSemicolon
-      r.replaceAllIn(line, m => StringEscapeUtils.escapeJava(m.group(1) ++ ";" ++ m.group(2)))
+      r.replaceAllIn(line, m => m.group(1) ++ ";" ++ m.group(2))
     }
   }
 
@@ -855,11 +855,10 @@ object Parser {
         "false" ^^^ PBoolLit(false) |
         "nil" ^^^ PNilLit() |
         regex("[0-9]+".r) ^^ (lit => PIntLit(BigInt(lit))) |
-        //runeLit |
         stringLit
 
     lazy val stringLit: Parser[PStringLit] =
-      rawStringLit | interpretedStringLit
+      (rawStringLit | interpretedStringLit) ^^ (strLit => PStringLit(StringEscapeUtils.escapeJava(strLit.lit)))
 
     lazy val rawStringLit: Parser[PStringLit] =
       // unicode characters and newlines are allowed

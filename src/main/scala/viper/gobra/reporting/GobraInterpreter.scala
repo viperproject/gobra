d82c5fdd85372e7d1f30d9402ea425abae50f0fa
Fabio Aliberti <fabioali@student.ethz.ch>
2021-04-14 14:09:19 +0200

    Product interpreter

--- src/main/scala/viper/gobra/reporting/GobraInterpreter.scala
+++ src/main/scala/viper/gobra/reporting/GobraInterpreter.scala
@@ -17,7 +17,8 @@ case class DefaultGobraInterpreter() extends GobraInterpreter{
   * @param c
   */
 case class MasterInterpreter(c:sil.Converter) extends GobraInterpreter{
-	val optionInterpreter = OptionInterpreter(c)
+	val optionInterpreter: GobraDomainInterpreter[OptionT] = OptionInterpreter(c)
+	val productInterpreter: GobraDomainInterpreter[StructT] = ProductInterpreter(c)
 	def interpret(entry:sil.ExtractedModelEntry,info:Type): GobraModelEntry ={
 		entry match{
 			case sil.LitIntEntry(v) => LitIntEntry(v)
@@ -26,6 +27,13 @@ case class MasterInterpreter(c:sil.Converter) extends GobraInterpreter{
 			case v:sil.VarEntry => interpret(c.extractVal(v),info)//TODO:make shure this does not pingpong
 			case d:sil.DomainValueEntry => info match {//TODO: More interpreters
 												case t:OptionT => optionInterpreter.interpret(d,t)
+												case t:StructT => productInterpreter.interpret(d,t);
+												case DeclaredT(d,c) => val name = d.left.name
+																		val actual = interpret(entry,c.symbType(d.right)) match {
+																			case l:LitEntry => l
+																			case _ => FaultEntry("not a lit entry")
+																		}
+																		LitDeclaredEntry(name,actual)
 												case _ => DummyEntry()
 											}
 			case sil.ExtendedDomainValueEntry(o,i) => interpret(o,info)
@@ -35,30 +43,63 @@ case class MasterInterpreter(c:sil.Converter) extends GobraInterpreter{
 	}
 }
 
-class OptionInterpreter(c:sil.Converter) extends GobraDomainInterpreter[OptionT]{
+case class OptionInterpreter(c:sil.Converter) extends GobraDomainInterpreter[OptionT]{
 	val nonFuncName :String = "optIsNone"//TODO: find out where they are generated
 	val getFuncName : String = "optGet"
-	def interpret(entry:sil.DomainValueEntry,typ:OptionT): GobraModelEntry = {
+	def interpret(entry:sil.DomainValueEntry,info:OptionT): GobraModelEntry = {
 		val doms = c.domains.filter(x=>x.valueName==entry.domain)
 		if(doms.length==1){
-			val functions = doms.head.functions
-			//TODO: make this safe
-			val noneFunc : sil.ExtractedFunction = functions.filter(_.fname==nonFuncName).head 
-			val getFunc : sil.ExtractedFunction= functions.filter(_.fname==getFuncName).head
+			val functions:Seq[sil.ExtractedFunction] =doms.head.functions
+			val noneFunc : sil.ExtractedFunction = functions.find(_.fname==nonFuncName) match {case Some(value) => value; case None => return FaultEntry(s"${entry}: could not relsove ($nonFuncName) not found")}
+			val getFunc : sil.ExtractedFunction= functions.find(_.fname==getFuncName) match {case Some(value) => value; case None => return FaultEntry(s"${entry}: could not relsove ($nonFuncName) not found")}
 			val isNone : Boolean = noneFunc.apply(Seq(entry)) match {
 				case Right(sil.LitBoolEntry(b)) => b
-				case _ => return FaultEntry(s"could not relsove option: ${entry}")
+				case _ => return FaultEntry(s"${entry}: could not relsove ($nonFuncName)")
 			}
 			if(isNone){
 				LitOptionEntry(None)
 			}else{
 				val actualval:sil.ExtractedModelEntry = getFunc.apply(Seq(entry)) match {
 					case Right(v) => v
-					case _ => return FaultEntry(s"could not relsove option: ${entry}")
+					case _ => return FaultEntry(s"${entry}: could not relsove ($getFuncName)")
 				}
-				val gobraval = viper.gobra.reporting.Util.valueTranslation(actualval,typ.elem,c)
+				val gobraval = MasterInterpreter(c).interpret(actualval,info.elem) match {
+					case e:LitEntry => e
+					case x => return FaultEntry(s"internal error: ${x}")
+				}
 				LitOptionEntry(Some(gobraval))
 			}
+		}else{
+			FaultEntry(s"could not relsove domain: ${entry.domain}")
+		}
+	}
+}
+case class ProductInterpreter(c:sil.Converter) extends GobraDomainInterpreter[StructT]{
+	def getterFunc(i:Int,n:Int) = s"get${i}of$n" //TODO: find out where they are generated
+	def interpret(entry:sil.DomainValueEntry,info:StructT) :GobraModelEntry ={
+		val doms = c.domains.find(_.valueName==entry.domain)
+		if(doms.isDefined){
+			val functions:Seq[sil.ExtractedFunction] =doms.get.functions
+			val fields = info.fields
+			val numFields = fields.size
+			val getterNames = Seq.range(0,numFields).map(getterFunc(_,numFields))
+			val getterfuncs = getterNames.map(x=>functions.find(_.fname==x)).collect({case Some(v)=> v})
+			val fieldToVals = (fields.keys.toSeq.zip(getterfuncs).map(
+												x=>(x._1,x._2.apply(Seq(entry)) match {case Right(v)=> v; 
+																				case _ => return FaultEntry(s"${entry}: could not relsove (${x._1})")
+																			})
+												)).toMap
+			try{
+			val values = fields.map(x=>(x._1,MasterInterpreter(c).interpret(fieldToVals.apply(x._1),x._2) match{
+																										case l:LitEntry=> l;
+																										case _ => return FaultEntry("internal error struct")
+																									}
+										))
+			LitStructEntry(info,values)
+			}catch{
+				case _ => return FaultEntry(s"$fieldToVals")
+			}
+			
 		}else{
 			FaultEntry(s"could not relsove domain: ${entry.domain}")
 		}

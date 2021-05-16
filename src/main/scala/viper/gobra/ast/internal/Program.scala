c2febbe523cf73852064eaff50ae686bb0f13fd3
xcwd <83215571+xcwd@users.noreply.github.com>
2021-05-17 05:58:54 +0800

    Update Program.scala

--- src/main/scala/viper/gobra/ast/internal/Program.scala
+++ src/main/scala/viper/gobra/ast/internal/Program.scala
@@ -252,7 +252,7 @@ case class Label(id: LabelProxy)(val info: Source.Parser.Info) extends Stmt
 
 case class If(cond: Expr, thn: Stmt, els: Stmt)(val info: Source.Parser.Info) extends Stmt
 
-case class While(cond: Expr, invs: Vector[Assertion], body: Stmt)(val info: Source.Parser.Info) extends Stmt
+case class While(cond: Expr, invs: Vector[Assertion], terminationMeasure: Option[Vector[Assertion]], body: Stmt)(val info: Source.Parser.Info) extends Stmt
 
 case class Initialization(left: AssignableVar)(val info: Source.Parser.Info) extends Stmt
 

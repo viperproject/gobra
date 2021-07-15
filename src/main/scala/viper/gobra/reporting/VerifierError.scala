f2a1f2d336a665a0ce7d0c184ade2362b9c88353
xcwd <83215571+xcwd@users.noreply.github.com>
2021-07-15 17:56:25 +0800

    Update VerifierError.scala

--- src/main/scala/viper/gobra/reporting/VerifierError.scala
+++ src/main/scala/viper/gobra/reporting/VerifierError.scala
@@ -266,6 +266,20 @@ case class ChannelSendError(info: Source.Verifier.Info) extends VerificationErro
   override def localMessage: String = s"The receive expression ${info.trySrc[PSendStmt](" ")}might fail"
 }
 
+case class FunctionTerminationError(info: Source.Verifier.Info) extends VerificationError{
+   override def localId: String = "function_termination_error"
+   override def localMessage: String = s"The function ${info.trySrc[PSendStmt](" ")}might not terminate"
+ }
+
+ case class MethodTerminationError(info: Source.Verifier.Info) extends VerificationError{
+   override def localId: String = "method_termination_error"
+   override def localMessage: String = s"The method ${info.trySrc[PSendStmt](" ")}might not terminate"
+ }
+
+case class LoopTerminationError (info: Source.Verifier.Info) extends VerificationError {
+   override def localId:  String = "Loop_termination_error"
+   override def localMessage: String = s"The loop ${info.trySrc[PSendStmt](" ")}might not terminate"
+ }
 
 
 sealed trait VerificationErrorReason {
@@ -368,6 +382,30 @@ case class GoCallPreconditionError(node: Source.Verifier.Info) extends Verificat
   override def id: String = "go_call_precondition_error"
   override def message: String = s"${node.origin.tag.trim} might not satisfy the precondition of the callee."
 }
+
+case class TerminationConditionFalseError(info:Source.Verifier.Info) extends VerificationErrorReason {
+   override def id:String = "termination_condition_false_error"
+   override def message: String = s"Required condition ${info.origin.tag.trim} for termination might not hold."
+ }
+ case class TupleConditionFalseError(info:Source.Verifier.Info) extends VerificationErrorReason {
+   override def id:String = "tuple_condition_false_error"
+   override def message: String = s"Required tuple condition ${info.origin.tag.trim} for termination might not hold."
+ }
+
+ case class TupleSimpleFalseError(info:Source.Verifier.Info) extends VerificationErrorReason {
+   override def id:String = "tuple_simple_false_error"
+   override def message: String = s"Termination measure ${info.origin.tag.trim} might not decrease or might not be bounded."
+ }
+
+ case class TupleDecreasesFalseError(info:Source.Verifier.Info) extends VerificationErrorReason {
+   override def id:String = "tuple_decreases_false_error"
+   override def message: String = s"Termination measure ${info.origin.tag.trim} might not decrease."
+ }
+
+ case class TupleBoundedFalseError(info:Source.Verifier.Info) extends VerificationErrorReason {
+   override def id:String = "tuple_bounded_false_error"
+   override def message: String = s"Termination measure ${info.origin.tag.trim} might not be bounded."
+ }
 
 sealed trait VerificationErrorClarification {
   def message: String

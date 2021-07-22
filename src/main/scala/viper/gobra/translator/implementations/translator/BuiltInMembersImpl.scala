29b6307164cc32d53fc9b61cc3a56aba83265a26
xcwd <83215571+xcwd@users.noreply.github.com>
2021-07-22 21:10:29 +0800

    Update BuiltInMembersImpl.scala

--- src/main/scala/viper/gobra/translator/implementations/translator/BuiltInMembersImpl.scala
+++ src/main/scala/viper/gobra/translator/implementations/translator/BuiltInMembersImpl.scala
@@ -204,7 +204,7 @@ class BuiltInMembersImpl extends BuiltInMembers {
         val pres: Vector[in.Assertion] = Vector(
           in.Access(isChannelInst, in.WildcardPerm(src))(src),
         )
-        in.PureMethod(recvParam, x.name, Vector(), Vector(kParam), pres, Vector(), None, None)(src)
+        in.PureMethod(recvParam, x.name, Vector(), Vector(kParam), pres, Vector(), Vector(), None)(src)
 
       case (tag: ChannelInvariantMethodTag, recv: in.ChannelT) =>
         /**
@@ -230,7 +230,7 @@ class BuiltInMembersImpl extends BuiltInMembers {
         val pres: Vector[in.Assertion] = Vector(
           in.Access(chanPredicate, in.WildcardPerm(src))(src)
         )
-        in.PureMethod(recvParam, x.name, Vector(), Vector(resParam), pres, Vector(), None, None)(src)
+        in.PureMethod(recvParam, x.name, Vector(), Vector(resParam), pres, Vector(), Vector(), None)(src)
 
       case (InitChannelMethodTag, recv: in.ChannelT) =>
         /**
@@ -284,7 +284,7 @@ class BuiltInMembersImpl extends BuiltInMembers {
           in.ExprAssertion(sendChannelInvEq)(src),
           in.ExprAssertion(recvChannelInvEq)(src),
         )
-        in.Method(recvParam, x.name, Vector(aParam, bParam), Vector(), pres, posts, None, None)(src)
+        in.Method(recvParam, x.name, Vector(aParam, bParam), Vector(), pres, posts, Vector(), None)(src)
 
       case (CreateDebtChannelMethodTag, recv: in.ChannelT) =>
         /**
@@ -312,7 +312,7 @@ class BuiltInMembersImpl extends BuiltInMembers {
           in.Access(closureDebtInst, in.FullPerm(src))(src),
           in.Access(tokenInst, in.FullPerm(src))(src),
         )
-        in.Method(recvParam, x.name, Vector(dividendParam, divisorParam /* permissionAmountParam */, predicateParam), Vector(), pres, posts, None, None)(src)
+        in.Method(recvParam, x.name, Vector(dividendParam, divisorParam /* permissionAmountParam */, predicateParam), Vector(), pres, posts, Vector(), None)(src)
 
       case (RedeemChannelMethodTag, recv: in.ChannelT) =>
         /**
@@ -335,7 +335,7 @@ class BuiltInMembersImpl extends BuiltInMembers {
           in.Access(closedInst, in.FullPerm(src))(src),
           in.Access(in.Accessible.PredExpr(in.PredExprInstance(predicateParam, Vector())(src)), in.FullPerm(src))(src)
         )
-        in.Method(recvParam, x.name, Vector(predicateParam), Vector(), pres, posts, None, None)(src)
+        in.Method(recvParam, x.name, Vector(predicateParam), Vector(), pres, posts, Vector(), None)(src)
 
       case (tag, recv) => violation(s"no method generation defined for tag $tag and receiver $recv")
     }
@@ -380,7 +380,7 @@ class BuiltInMembersImpl extends BuiltInMembers {
         val posts: Vector[in.Assertion] = Vector(
           in.Access(closedInst, in.FullPerm(src))(src)
         )
-        in.Function(x.name, args, Vector(), pres, posts,None, None)(src)
+        in.Function(x.name, args, Vector(), pres, posts, Vector(), None)(src)
       case (tag, args) => violation(s"no function generation defined for tag $tag and arguments $args")
     }
   }

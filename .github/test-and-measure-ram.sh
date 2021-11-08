446f914d81bed07e2d39affdfe689f971b320e81
Linard Arquint <linard.arquint@inf.ethz.ch>
2021-11-08 17:16:22 +0100

    creates a workflow that runs tests and builds docker image

---
+++ .github/test-and-measure-ram.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# start pidstats and write output to synced folder /build/gobra/sync
+pidstat 1 -r -p ALL > /build/gobra/sync/pidstat.txt & PIDSTAT_PID=$!
+
+# execute sbt test and stop pidstat independent of outcome
+sbt test
+# java -Xss128m -jar /build/gobra/target/scala-2.13/gobra.jar -i /build/gobra/src/test/resources/regressions/examples/swap.gobra
+TEST_RES=$?
+kill -INT $PIDSTAT_PID
+
+# the following command would execute the precompiled tests:
+# java -Xss128m -XshowSettings:vm -cp target/scala-2.13/gobra-test.jar -Dlogback.configurationFile=conf/logback.xml -DGOBRATESTS_REGRESSIONS_DIR=src/test/resources/regressions -DGOBRATESTS_SAME_PACKAGE_DIR=src/test/resources/same_package org.scalatest.tools.Runner -R target/scala-2.13/test-classes -o
+
+# set exit code depending on result of `sbt test`:
+exit $TEST_RES

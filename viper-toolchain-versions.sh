b95430fcdefc548975726592d197b20124d8554e
Linard Arquint <linard.arquint@inf.ethz.ch>
2021-11-15 17:54:49 +0100

    fixes license headers

--- viper-toolchain-versions.sh
+++ viper-toolchain-versions.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2011-2021 ETH Zurich.
 
 SILVER_REF="f645539a04e76d70337d7b5b108e7f9a9de9bc7a"
 SILICON_REF="v.21.07-release"

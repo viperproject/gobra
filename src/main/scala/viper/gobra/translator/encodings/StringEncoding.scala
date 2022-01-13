abf85a2b08f9ca92d4f708eb66dafbca11ae23b5
João Pereira <joaopereira.19@gmail.com>
2022-01-13 09:49:58 +0100

    Add Lino's stubs to master (#385)
    * Add Lino's stubs to master
    
    * Update src/main/resources/stubs/time/sleep.gobra
    
    Co-authored-by: Linard Arquint <ArquintL@users.noreply.github.com>
    
    * implement Linard's feedback
    
    Co-authored-by: Linard Arquint <ArquintL@users.noreply.github.com>

--- src/main/scala/viper/gobra/translator/encodings/StringEncoding.scala
+++ src/main/scala/viper/gobra/translator/encodings/StringEncoding.scala
@@ -267,6 +267,7 @@ class StringEncoding extends LeafTypeEncoding {
 
   /** Generates the function
     *   requires forall i int :: 0 <= i && i < len(s) ==> acc(&s[i], _)
+    *   decreases _
     *   pure func byteSliceToStrFunc(s []byte) string
     */
   private val byteSliceToStrFuncName: String = "byteSliceToStrFunc"
@@ -292,7 +293,7 @@ class StringEncoding extends LeafTypeEncoding {
         results = Vector(res),
         pres = Vector(pre),
         posts = Vector(),
-        terminationMeasures = Vector(),
+        terminationMeasures = Vector(in.WildcardMeasure(None)(info)),
         body = None
       )(info)
       val translatedFunc = ctx.pureMethod.pureFunction(func)(ctx)

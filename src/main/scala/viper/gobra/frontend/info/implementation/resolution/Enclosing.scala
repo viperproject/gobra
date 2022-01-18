4daf0260241d76db74d48bbe55476ece15d0e004
João Pereira <joaopereira.19@gmail.com>
2022-01-18 13:43:54 +0100

    Incorporate feedback from Felix

--- src/main/scala/viper/gobra/frontend/info/implementation/resolution/Enclosing.scala
+++ src/main/scala/viper/gobra/frontend/info/implementation/resolution/Enclosing.scala
@@ -42,7 +42,7 @@ trait Enclosing { this: TypeInfoImpl =>
   lazy val isEnclosingExplicitGhost: PNode => Boolean =
     down(false){ case _: PGhostifier[_] => true }
 
-  lazy val isEnclosingtGhost: PNode => Boolean =
+  lazy val isEnclosingGhost: PNode => Boolean =
     down(false){ case _: PGhostifier[_] | _: PGhostNode => true }
 
   lazy val isEnclosingDomain: PNode => Boolean =

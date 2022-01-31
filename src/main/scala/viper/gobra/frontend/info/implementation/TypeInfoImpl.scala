00b1cbf2f4baf745d4eec29ac717f3556901ae45
João Pereira <joaopereira.19@gmail.com>
2022-01-31 21:28:55 +0100

    Backup work

--- src/main/scala/viper/gobra/frontend/info/implementation/TypeInfoImpl.scala
+++ src/main/scala/viper/gobra/frontend/info/implementation/TypeInfoImpl.scala
@@ -112,8 +112,50 @@ class TypeInfoImpl(final val tree: Info.GoTree, final val context: Info.Context,
     res
   }
 
+  // TODO: computes every node that might contain a visible invocation to the importing package
+  private def relevantSubnodes(n: PNode): Vector[PNode] = n match {
+    // predicates, methods, pure methods
+    case decl@ PFunctionDecl(id, args, result, spec, _) =>
+      if (decl.spec.isPure) tree.child(decl) else id +: result +: spec +: args
+    case sig:  PMethodSig => tree.child(sig)
+    case decl@ PMethodDecl(id, receiver, args, result, spec, _) =>
+      if (decl.spec.isPure) tree.child(decl) else id +: receiver +: result +: spec +: args
+    case decl: PMPredicateDecl => tree.child(decl)
+    case decl: PFPredicateDecl => tree.child(decl)
+    case impl: PMethodImplementationProof => tree.child(impl)
+    case decl: PTypeDecl => tree.child(decl.right)
+    case _ => Vector()
+  }
+
+  // Todo: document
+  private lazy val usedMembers: Set[PNode] = {
+    var visited: Set[PNode] = Set()
+    // TODO: change to stack
+    var workSet: Vector[PNode] = externallyAccessedMembers
+    //println(s"set: $externallyAccessedMembers")
+    while (workSet.nonEmpty) {
+      val elem = workSet.head
+      val tail = workSet.tail
+      var newNodes: Vector[PNode] = Vector()
+
+      if (!visited.contains(elem)) {
+        visited = Set(elem) ++ visited
+        newNodes = relevantSubnodes(elem).flatMap(allChildren).flatMap {
+          case n: PExpressionOrType => resolve(n) match {
+            case Some(s: AstPattern.Symbolic) => Vector(s.symb.rep)
+            case _ => Vector()
+          }
+          case _ => Vector()
+        }
+      }
+      workSet = tail ++ newNodes
+    }
+
+    visited
+  }
+
   override def isUsed(m: PMember): Boolean = {
-    externallyAccessedMembers.contains(m)
+    usedMembers.contains(m)
   }
 
   override def struct(n: PNode): Option[Type.StructT] =

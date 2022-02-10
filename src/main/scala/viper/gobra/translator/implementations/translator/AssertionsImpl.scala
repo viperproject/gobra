2cc3784bb5c5febdddeba1d4de8c04e528ceea4b
João Pereira <joaopereira.19@gmail.com>
2022-02-10 17:49:51 +0100

    fix warning

--- src/main/scala/viper/gobra/translator/implementations/translator/AssertionsImpl.scala
+++ src/main/scala/viper/gobra/translator/implementations/translator/AssertionsImpl.scala
@@ -43,7 +43,7 @@ class AssertionsImpl extends Assertions {
         acc.e match {
           case in.Accessible.Predicate(op) => ctx.predicate.predicateAccess(ctx)(op, acc.p)
           case in.Accessible.Address(op) => ctx.typeEncoding.addressFootprint(ctx)(op, acc.p)
-          case in.Accessible.ExprAccess(op) => ctx.typeEncoding.assertion(ctx)(acc)
+          case in.Accessible.ExprAccess(_) => ctx.typeEncoding.assertion(ctx)(acc)
           case n => Violation.violation(s"node $n should have been handled by an type encoding.")
         }
 

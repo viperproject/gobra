22ec688bcf7bb91935b0894fb9b3a9af8e6b1e36
João Pereira <joaopereira.19@gmail.com>
2022-08-05 11:26:17 +0200

    Add license

--- genparser.sh
+++ genparser.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2011-2022 ETH Zurich.
 
 # This script generates a new parser from the antlr4 files stored in this repository.
 # This script MUST NOT be run from a symlink.

f09b80b67412125b2979e4b22e1f061014833266
Stefano Milizia <stefano.milizia00@gmail.com>
2022-08-06 16:10:46 +0100

    Make closure calls deferrable

--- src/main/scala/viper/gobra/translator/util/Core.scala
+++ src/main/scala/viper/gobra/translator/util/Core.scala
@@ -25,6 +25,7 @@ object Core {
   def core(x: in.Deferrable): Core[in.Deferrable] = x match {
     case x: in.FunctionCall => for (args <- vector(x.args map core)) yield x.copy(args = args)(x.info)
     case x: in.MethodCall => for (recv <- core(x.recv); args <- vector(x.args map core)) yield x.copy(recv = recv, args = args)(x.info)
+    case x: in.ClosureCall => for (closure <- core(x.closure); args <- vector(x.args map core); spec <- deepCore(x.spec)) yield x.copy(closure = closure, args = args, spec = spec)(x.info)
     case x: in.Fold => for (acc <- core(x.acc)) yield x.copy(acc = acc)(x.info)
     case x: in.Unfold => for (acc <- core(x.acc)) yield x.copy(acc = acc)(x.info)
     case x: in.PredExprFold => for (base <- deepCore(x.base); args <- vector(x.args map core)) yield x.copy(base = base, args = args)(x.info)
@@ -62,11 +63,19 @@ object Core {
     } yield x.copy(args = args)(x.info)
   }
 
+  def deepCore(x: in.ClosureSpec): Core[in.ClosureSpec] = {
+    for {
+      paramValues <- vector(x.paramValues.map(y => option(y.map((core)))))
+      params = paramValues.zipWithIndex.collect{ case (Some(exp), idx) => idx+1 -> exp }.toMap
+    } yield x.copy(params = params)(x.info)
+  }
+
 
   /** Returns all expressions of a statement such that `core(s).run(coreArgs(s)) == s` */
   def coreArgs(x: in.Deferrable): Vector[in.Expr] = x match {
     case x: in.FunctionCall => x.args
     case x: in.MethodCall => x.recv +: x.args
+    case x: in.ClosureCall => (x.closure +: x.args) ++ deepCoreArgs(x.spec)
     case x: in.Fold => coreArgs(x.acc)
     case x: in.Unfold => coreArgs(x.acc)
     case x: in.PredExprFold => deepCoreArgs(x.base) ++ x.args.flatMap(coreArgs)
@@ -93,6 +102,8 @@ object Core {
   def coreArgs(x: in.Expr): Vector[in.Expr] = Vector(x)
 
   def deepCoreArgs(x: in.PredicateConstructor): Vector[in.Expr] = x.args.flatten
+
+  def deepCoreArgs(x: in.ClosureSpec): Vector[in.Expr] = x.paramValues.flatten
 }
 
 case class Core[+R](gen: Vector[in.LocalVar] => (R, Vector[in.LocalVar])) {

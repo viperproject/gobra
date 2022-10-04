df25ef010ca8e54ce1a18d8979c2d880ff790318
João Pereira <joaopereira.19@gmail.com>
2022-10-04 13:50:20 +0200

    Test constant propagation

--- src/main/scala/viper/gobra/Gobra.scala
+++ src/main/scala/viper/gobra/Gobra.scala
@@ -14,7 +14,7 @@ import com.typesafe.scalalogging.StrictLogging
 import org.slf4j.LoggerFactory
 import viper.gobra.ast.frontend.PPackage
 import viper.gobra.ast.internal.Program
-import viper.gobra.ast.internal.transform.{CGEdgesTerminationTransform, InternalTransform, OverflowChecksTransform}
+import viper.gobra.ast.internal.transform.{CGEdgesTerminationTransform, ConstantPropagation, InternalTransform, OverflowChecksTransform}
 import viper.gobra.backend.BackendVerifier
 import viper.gobra.frontend.info.{Info, TypeInfo}
 import viper.gobra.frontend.{Config, Desugar, PackageInfo, Parser, ScallopGobraConfig}
@@ -266,7 +266,7 @@ class Gobra extends GoVerifier with GoIdeVerifier {
     * be easily extended to perform more transformations
     */
   private def performInternalTransformations(program: Program, config: Config, pkgInfo: PackageInfo): Either[Vector[VerifierError], Program] = {
-    var transformations: Vector[InternalTransform] = Vector(CGEdgesTerminationTransform)
+    var transformations: Vector[InternalTransform] = Vector(CGEdgesTerminationTransform, ConstantPropagation)
     if (config.checkOverflows) {
       transformations :+= OverflowChecksTransform
     }
---
+++ src/main/scala/viper/gobra/ast/internal/transform/ConstantPropagation.scala
@@ -0,0 +1,38 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2011-2021 ETH Zurich.
+
+package viper.gobra.ast.internal.transform
+
+import viper.gobra.ast.{internal => in}
+import viper.gobra.util.Violation
+
+object ConstantPropagation extends InternalTransform {
+  override def name(): String = "constant_propagation"
+
+  /**
+    * Program-to-program transformation
+    */
+  override def transform(p: in.Program): in.Program = {
+    val (constDecls, noConstDecls) = p.members partition(_.isInstanceOf[in.GlobalConstDecl])
+    val progNoConsts = in.Program(
+      types = p.types,
+      members = noConstDecls,
+      table = p.table,
+    )(p.info)
+
+    println(p.table.getMPredicates.forall(x => noConstDecls.contains(x)))
+    println(p.table.getFPredicates.forall(x => noConstDecls.contains(x)))
+    p.table
+    progNoConsts transform {
+      case c: in.GlobalConst =>
+        val litOpt = constDecls collectFirst { case in.GlobalConstDecl(l, r) if l == c => r.withInfo(c.info) }
+        litOpt match {
+          case Some(l) => l
+          case _ => Violation.violation(s"Did not find declaration of constant $c")
+        }
+    }
+  }
+}

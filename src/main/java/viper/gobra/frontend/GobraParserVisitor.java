a13e324e28794a871097ae306f8b5cfc8f82bf73
Colin Pfingstl <colin.pfingstl@hotmail.ch>
2023-08-24 19:59:39 +0200

    regenerate parser after merge

--- src/main/java/viper/gobra/frontend/GobraParserVisitor.java
+++ src/main/java/viper/gobra/frontend/GobraParserVisitor.java
@@ -1,4 +1,4 @@
-// Generated from src/main/antlr4/GobraParser.g4 by ANTLR 4.12.0
+// Generated from S:/GitHub/gobra/src/main/antlr4\GobraParser.g4 by ANTLR 4.12.0
 package viper.gobra.frontend;
 import org.antlr.v4.runtime.tree.ParseTreeVisitor;
 

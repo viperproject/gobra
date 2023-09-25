a35a8b547aa1deb29a50515f7635cbdf1e91c283
Felix A. Wolf <felix.wolf@inf.ethz.ch>
2023-09-25 23:34:40 +0200

    major only

--- src/main/scala/viper/gobra/translator/transformers/hyper/SIFLowGuardTransformer.scala
+++ src/main/scala/viper/gobra/translator/transformers/hyper/SIFLowGuardTransformer.scala
@@ -1,6 +1,7 @@
 package viper.gobra.translator.transformers.hyper
 
 import viper.gobra.backend.BackendVerifier
+import viper.gobra.reporting.Source
 import viper.gobra.translator.transformers.ViperTransformer
 import viper.silver.ast._
 import viper.silver.ast.utility.Simplifier
@@ -12,13 +13,12 @@ trait SIFLowGuardTransformer {
 
   def program(
                  p: Program,
-                 noNI: Method => Boolean,
-                 noDups: String => Boolean,
+                 onlyMajor: String => Boolean,
                ): Program = {
 
     val relPreds = relationalPredicates(p)
-    val moreNoDups = noDuplicates(p)
-    val ctx = newContext(relPreds, s => noDups(s) || moreNoDups(s), noNI)
+    val noDups = noDuplicates(p)
+    val ctx = newContext(relPreds, noDups, onlyMajor)
 
     val members =
       p.methods.flatMap(method(_, ctx)) ++
@@ -37,7 +37,7 @@ trait SIFLowGuardTransformer {
   }
 
   def method(m: Method, ctx: Context): Vector[Member] = {
-    if (ctx.noNI(m)) {
+    if (ctx.onlyMajor(m.name)) {
       Vector(
         m.copy(
           formalArgs = m.formalArgs.map(runLocalVarDecl(_, ctx.major)),
@@ -61,7 +61,7 @@ trait SIFLowGuardTransformer {
   }
 
   def function(f: Function, ctx: Context): Vector[Member] = {
-    if (ctx.hasNoDuplicates(f.name)) {
+    if (ctx.hasNoDuplicates(f.name) || ctx.onlyMajor(f.name)) {
       Vector(runFunction(f, ctx.major))
     } else Vector(runFunction(f, ctx.major), runFunction(f, ctx.minor))
   }
@@ -77,33 +77,37 @@ trait SIFLowGuardTransformer {
   }
 
   def predicate(p: Predicate, ctx: Context): Vector[Member] = {
-    val major = runPredicate(p, ctx.major)
-    val minor = runPredicate(p, ctx.minor)
+    if (ctx.onlyMajor(p.name)) {
+      Vector(runPredicate(p, ctx.major))
+    } else {
 
-    p.body match {
-      case Some(body) if ctx.isRelationalPredicate(p.name) =>
-        val args = p.formalArgs.map(_.localVar)
-        val relBody = assertionToRelational(body, ctx)
-        val majorAccess = runPredicateAccess(p.name, args, ctx.major)(body.pos, body.info, body.errT)
-        val minorAccess = runPredicateAccess(p.name, args, ctx.minor)(body.pos, body.info, body.errT)
-        val majorAccessPredicate = PredicateAccessPredicate(majorAccess, WildcardPerm()(body.pos, body.info, body.errT))(body.pos, body.info, body.errT)
-        val minorAccessPredicate = PredicateAccessPredicate(minorAccess, WildcardPerm()(body.pos, body.info, body.errT))(body.pos, body.info, body.errT)
+      val major = runPredicate(p, ctx.major)
+      val minor = runPredicate(p, ctx.minor)
 
-        val func = Function(
-          name = ctx.predicateFunctionName(p.name),
-          formalArgs = p.formalArgs.map(runLocalVarDecl(_, ctx.major)) ++ p.formalArgs.map(runLocalVarDecl(_, ctx.minor)),
-          typ = Bool,
-          pres = Seq(majorAccessPredicate, minorAccessPredicate),
-          posts = Seq.empty,
-          body = Some(
-            Unfolding(majorAccessPredicate, Unfolding(minorAccessPredicate, relBody)(body.pos, body.info, body.errT))(body.pos, body.info, body.errT)
-          )
-        )()
+      p.body match {
+        case Some(body) if ctx.isRelationalPredicate(p.name) =>
+          val args = p.formalArgs.map(_.localVar)
+          val relBody = assertionToRelational(body, ctx)
+          val majorAccess = runPredicateAccess(p.name, args, ctx.major)(body.pos, body.info, body.errT)
+          val minorAccess = runPredicateAccess(p.name, args, ctx.minor)(body.pos, body.info, body.errT)
+          val majorAccessPredicate = PredicateAccessPredicate(majorAccess, WildcardPerm()(body.pos, body.info, body.errT))(body.pos, body.info, body.errT)
+          val minorAccessPredicate = PredicateAccessPredicate(minorAccess, WildcardPerm()(body.pos, body.info, body.errT))(body.pos, body.info, body.errT)
 
-        Vector(major, minor, func)
+          val func = Function(
+            name = ctx.predicateFunctionName(p.name),
+            formalArgs = p.formalArgs.map(runLocalVarDecl(_, ctx.major)) ++ p.formalArgs.map(runLocalVarDecl(_, ctx.minor)),
+            typ = Bool,
+            pres = Seq(majorAccessPredicate, minorAccessPredicate),
+            posts = Seq.empty,
+            body = Some(
+              Unfolding(majorAccessPredicate, Unfolding(minorAccessPredicate, relBody)(body.pos, body.info, body.errT))(body.pos, body.info, body.errT)
+            )
+          )()
 
-      case _ => Vector(major, minor)
+          Vector(major, minor, func)
 
+        case _ => Vector(major, minor)
+      }
     }
   }
 
@@ -116,7 +120,7 @@ trait SIFLowGuardTransformer {
   }
 
   def field(f: Field, ctx: Context): Vector[Member] = {
-    if (ctx.hasNoDuplicates(f.name)) {
+    if (ctx.hasNoDuplicates(f.name) || ctx.onlyMajor(f.name)) {
       Vector(runField(f, ctx.major))
     } else Vector(runField(f, ctx.major), runField(f, ctx.minor))
   }
@@ -126,7 +130,6 @@ trait SIFLowGuardTransformer {
   }
 
   def domain(d: Domain, @unused ctx: Context): Vector[Member] = Vector(d)
-
 
   def directlyRelational(e: Exp): Boolean = {
     e.exists {
@@ -244,7 +247,9 @@ trait SIFLowGuardTransformer {
   def runExp(e: Exp, ctx: RunContext): Exp = {
     def go(e: Exp): Exp = runExp(e, ctx)
 
-    e.transform {
+    val reducedE = removeMinorExp(e, ctx)
+
+    reducedE.transform {
       case v: LocalVar => v.copy(name = ctx.rename(v.name))(v.pos, v.info, v.errT)
       case v: LocalVarDecl => v.copy(name = ctx.rename(v.name))(v.pos, v.info, v.errT)
       case f: Field => runField(f, ctx)
@@ -252,6 +257,52 @@ trait SIFLowGuardTransformer {
       case p: PredicateAccess => runPredicateAccess(p.predicateName, p.args, ctx)(p.pos, p.info, p.errT)
       case l: SIFLowExp => TrueLit()(l.pos, l.info, l.errT)
       case l: SIFLowEventExp => TrueLit()(l.pos, l.info, l.errT)
+    }
+  }
+
+  def hasMajorOnly(n: Node, ctx: Context): Boolean = n.exists {
+    case v: LocalVar => ctx.onlyMajor(v.name)
+    case v: LocalVarDecl => ctx.onlyMajor(v.name)
+    case f: Field => ctx.onlyMajor(f.name)
+    case f: FuncApp => ctx.onlyMajor(f.funcname)
+    case f: DomainFuncApp => ctx.onlyMajor(f.funcname)
+    case p: PredicateAccess => ctx.onlyMajor(p.predicateName)
+    case m: MethodCall => ctx.onlyMajor(m.methodName)
+    case _ => false
+  }
+
+  def removeMinorExp(e: Exp, ctx: RunContext): Exp = {
+    if (ctx.isMajor || e.typ != Bool) e
+    else {
+
+      def go(e: Exp): Reduction[Exp] = e match {
+        case e: And =>
+          (go(e.left), go(e.right)) match {
+            case (Reduction.Inf, y) => y.increment
+            case (x, Reduction.Inf) => x.increment
+            case (x, y) => for {l <- x; r <- y} yield And(l, r)(e.pos, e.info, e.errT)
+          }
+
+        case e: Or => for {l <- go(e.left).strict; r <- go(e.right).strict} yield Or(l,r)(e.pos, e.info, e.errT)
+        case e: Implies => for {l <- go(e.left).strict; r <- go(e.right)} yield Implies(l,r)(e.pos, e.info, e.errT)
+
+        case e: CondExp =>
+          (go(e.cond).strict, go(e.thn), go(e.els)) match {
+            case (Reduction.Zero(c), Reduction.Inf, y) => for {r <- y} yield Implies(c,r)(e.pos, e.info, e.errT)
+            case (Reduction.Zero(c), x, Reduction.Inf) => for {l <- x} yield Implies(Not(c)(e.pos, e.info, e.errT),l)(e.pos, e.info, e.errT)
+            case (z, x, y) => for {c <- z; l <- x; r <- y} yield CondExp(c,l,r)(e.pos, e.info, e.errT)
+          }
+
+        case e: Forall => for { body <- go(e.exp) } yield e.copy(exp = body)(e.pos, e.info, e.errT)
+        case e: Exists => for { body <- go(e.exp) } yield e.copy(exp = body)(e.pos, e.info, e.errT)
+
+        case e: Unfolding => for { x <- go(e.acc); y <- go(e.body) } yield Unfolding(x.asInstanceOf[PredicateAccessPredicate], y)(e.pos, e.info, e.errT)
+
+        case e if e.typ == Bool && hasMajorOnly(e,ctx) => Reduction.Inf // atomic exp with removable sub-exp
+        case e => Reduction.Zero(e)
+      }
+
+      go(e).toOption.getOrElse(TrueLit()(e.pos, e.info, e.errT))
     }
   }
 
@@ -268,7 +319,8 @@ trait SIFLowGuardTransformer {
       case a: Assert => isRelational(a.exp, ctx)
       case f: Fold => isRelational(f.acc.loc, ctx)
       case u: Unfold => isRelational(u.acc.loc, ctx)
-      case _: MethodCall | _: If | _: While | _: Goto | _: Label | _: Seqn => true
+      case m: MethodCall => !ctx.onlyMajor(m.methodName)
+      case _: If | _: While | _: Goto | _: Label | _: Seqn => true
       case _ => false
     }
 
@@ -324,13 +376,7 @@ trait SIFLowGuardTransformer {
       case s: While =>
         While(
           cond = runExp(s.cond, ctx.major),
-          invs = s.invs.map(assertion(_, ctx)),
-          body = toSeqn(statement(s.body, ctx))
-        )(s.pos, s.info, s.errT)
-
-        While(
-          cond = runExp(s.cond, ctx.major),
-          invs = s.invs.map(assertion(_, ctx)),
+          invs = comparison(SIFLowExp(s.cond)(s.pos, s.info, s.errT), ctx) +: s.invs.map(assertion(_, ctx)),
           body = toSeqn(statement(s.body, ctx))
         )(s.pos, s.info, s.errT)
 
@@ -345,17 +391,23 @@ trait SIFLowGuardTransformer {
   }
 
   def runStatement(s: Stmt, ctx: RunContext): Stmt = {
-    s.transform {
+    val reducedStmt = removeMinorStmt(s, ctx)
+    reducedStmt.transform {
       case v: LocalVarDecl => v.copy(name = ctx.rename(v.name))(v.pos, v.info, v.errT)
       case f: Field => runField(f, ctx)
       case e: Exp => runExp(e, ctx)
     }
   }
 
+  def removeMinorStmt(s: Stmt, ctx: RunContext): Stmt = {
+    if (ctx.isMajor || !hasMajorOnly(s, ctx)) s
+    else Seqn(Seq.empty, Seq.empty)(s.pos, s.info, s.errT)
+  }
+
   def newContext(
                   _relPreds: String => Boolean,
-                  _noDubs: String => Boolean,
-                  _noNI: Method => Boolean,
+                  _noDups: String => Boolean,
+                  _onlyMajor: String => Boolean,
                 ): Context = {
 
     trait MajorContext extends RunContext { self: Context =>
@@ -371,10 +423,12 @@ trait SIFLowGuardTransformer {
     }
 
     trait ContextImpl extends Context {
+      override def onlyMajor(n: String): Boolean = _onlyMajor(n)
+
       override def isRelationalPredicate(p: String): Boolean = _relPreds(p)
       override def predicateFunctionName(n: String): String = s"${n}F"
-      override def hasNoDuplicates(n: String): Boolean = _noDubs(n)
-      override def noNI(m: Method): Boolean = _noNI(m)
+      override def hasNoDuplicates(n: String): Boolean = _noDups(n)
+
       override def major: RunContext = new ContextImpl with MajorContext
       override def minor: RunContext = new ContextImpl with MinorContext
     }
@@ -389,10 +443,11 @@ trait SIFLowGuardTransformer {
 }
 
 trait Context {
+  def onlyMajor(@unused n: String): Boolean
+
   def isRelationalPredicate(@unused p: String): Boolean
   def predicateFunctionName(@unused n: String): String
   def hasNoDuplicates(@unused n: String): Boolean
-  def noNI(@unused m: Method): Boolean
 
   def major: RunContext
   def minor: RunContext
@@ -403,16 +458,62 @@ trait RunContext extends Context {
   def rename(@unused x: String): String
 }
 
-class SIFLowGuardTransformerImpl extends SIFLowGuardTransformer with ViperTransformer {
+sealed trait Reduction[+T] {
+  def toOption: Option[T]
+  def increment: Reduction[T] = toOption match {
+    case None => Reduction.Inf
+    case Some(x) => Reduction.One(x)
+  }
+  def strict: Reduction[T]
+  def map[Q](f: T => Q): Reduction[Q]
+  def flatMap[Q](f: T => Reduction[Q]): Reduction[Q]
+}
 
-  def noNI(m: Method): Boolean = m match {
-    case m if m.name.endsWith("termination_proof") => true
-    case _ => false
+object Reduction {
+  case class Zero[T](x: T) extends Reduction[T] {
+    override def toOption: Option[T] = Some(x)
+    override def strict: Reduction[T] = Zero(x)
+    override def map[Q](f: T => Q): Reduction[Q] = Zero(f(x))
+    override def flatMap[Q](f: T => Reduction[Q]): Reduction[Q] = f(x)
   }
 
-  def noDup(@unused n: String): Boolean = false
+  case class One[T](x: T) extends Reduction[T] {
+    override def toOption: Option[T] = Some(x)
+    override def strict: Reduction[T] = Inf
+    override def map[Q](f: T => Q): Reduction[Q] = One(f(x))
+    override def flatMap[Q](f: T => Reduction[Q]): Reduction[Q] = f(x) match {
+      case Zero(y) => One(y)
+      case r => r
+    }
+  }
+
+  case object Inf extends Reduction[Nothing] {
+    override def toOption: Option[Nothing] = None
+    override def strict: Reduction[Nothing] = Inf
+    override def map[Q](f: Nothing => Q): Reduction[Q] = Inf
+    override def flatMap[Q](f: Nothing => Reduction[Q]): Reduction[Q] = Inf
+  }
+}
+
+class SIFLowGuardTransformerImpl extends SIFLowGuardTransformer with ViperTransformer {
+
+  def onlyMajor(p: Program): String => Boolean = {
+    def nameBased(n: String): Boolean = n match {
+      case n if n.endsWith("termination_proof") => true
+      case _ => false
+    }
+
+    val annotationBased = p.members.flatMap{ m =>
+      Source.unapply(m).flatMap{ code =>
+        if (code.origin.tag.contains("#[MAJOR]")) Some(m.name)
+        else None
+      }
+    }.toSet
+
+    (x: String) => nameBased(x) || annotationBased.contains(x)
+  }
 
   override def transform(task: BackendVerifier.Task): Either[Seq[AbstractError], BackendVerifier.Task] = {
-    Right(BackendVerifier.Task(program(task.program, noNI, noDup), task.backtrack))
+    Right(BackendVerifier.Task(program(task.program, onlyMajor(task.program)), task.backtrack))
   }
 }

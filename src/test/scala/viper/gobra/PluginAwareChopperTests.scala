e0b20f19a06d4858fc69e038d700d5d677dd9481
João Pereira <joaopereira.19@gmail.com>
2025-01-12 20:35:38 +0100

    added missing header

--- src/test/scala/viper/gobra/PluginAwareChopperTests.scala
+++ src/test/scala/viper/gobra/PluginAwareChopperTests.scala
@@ -1,3 +1,8 @@
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2025 ETH Zurich.
+
 package viper.gobra
 
 import org.scalatest.Inside

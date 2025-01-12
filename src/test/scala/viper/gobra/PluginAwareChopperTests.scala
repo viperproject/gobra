179d2ab8589152246034e5c3376265b70f1f93cc
João Pereira <joaopereira.19@gmail.com>
2025-01-12 20:30:52 +0100

    Add plugin-aware chopper

---
+++ src/test/scala/viper/gobra/PluginAwareChopperTests.scala
@@ -0,0 +1,27 @@
+package viper.gobra
+
+import org.scalatest.Inside
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import viper.gobra.util.PluginAwareChopper
+import viper.silver.ast
+import viper.silver.ast.utility.chopper.Penalty
+
+class PluginAwareChopperTests extends AnyFunSuite with Matchers with Inside {
+  test("Domains ending in WellFoundedOrders are kept in chopped programs") {
+    val intVarDecl = ast.LocalVarDecl("i", ast.Int)()
+    val function = ast.Function("functionA", Seq(intVarDecl), ast.Bool, Seq.empty, Seq.empty, None)()
+    val domainName = "IntWellFoundedOrder"
+    val decreasingFn = ast.DomainFunc(
+      "decreasing",
+      Seq(ast.LocalVarDecl("l1", ast.Int)(), ast.LocalVarDecl("l2", ast.Int)()),
+      ast.Bool,
+    )(domainName = domainName)
+    val boundedFn = ast.DomainFunc("bounded", Seq(ast.LocalVarDecl("l", ast.Int)()), ast.Bool)(domainName = domainName)
+    val wfoDomain = ast.Domain(domainName, Seq(decreasingFn, boundedFn), Seq())()
+    val program = ast.Program(Seq(wfoDomain), Seq.empty, Seq(function), Seq.empty, Seq.empty, Seq.empty)()
+    val result = PluginAwareChopper.chop(program)(bound = Some(5), penalty = Penalty.DefaultWithoutForcedMerge)
+    result.length shouldBe 1
+    result.head shouldEqual program
+  }
+}

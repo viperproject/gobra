c2e2f0eb7790f444e70789724144600d4d728b60
Linard Arquint <linard.arquint@inf.ethz.ch>
2025-03-31 14:47:15 +0200

    passes same Gobra instance and execution context to testcases belonging to the same test suite

--- src/test/scala/viper/gobra/AbstractGobraTests.scala
+++ src/test/scala/viper/gobra/AbstractGobraTests.scala
@@ -6,18 +6,39 @@
 
 package viper.gobra
 
+import org.scalatest.{BeforeAndAfterAll, Tag}
+import viper.gobra.util.{DefaultGobraExecutionContext, GobraExecutionContext}
+
 import java.io.File
 import java.nio.file.Path
-import viper.silver.testing.{AnnotationBasedTestSuite, DefaultAnnotatedTestInput, DefaultTestInput, TestAnnotationParser, TestInput}
+import viper.silver.testing.{AnnotatedTestInput, AnnotationBasedTestSuite, DefaultTestInput, ProjectInfo, TestAnnotationParser, TestAnnotations, TestInput}
 import viper.silver.utility.Paths
 
-abstract class AbstractGobraTests extends AnnotationBasedTestSuite {
+abstract class AbstractGobraTests extends AnnotationBasedTestSuite with BeforeAndAfterAll {
 
   val specificationCommentStart = "//@"
 
   val z3PropertyName = "GOBRATESTS_Z3_EXE"
 
   val z3Exe: Option[String] = Option(System.getProperty(z3PropertyName))
+
+  private var _prerequisites: Option[(Gobra, GobraExecutionContext)] = None
+  def prerequisites(): (Gobra, GobraExecutionContext) = {
+    val res = _prerequisites.getOrElse(new Gobra(), new DefaultGobraExecutionContext())
+    _prerequisites = Some(res)
+    res
+  }
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    prerequisites() // we invoke `prerequisites()` for its side-effects
+  }
+
+  override def afterAll(): Unit = {
+    _prerequisites.foreach { case (_, ctx) => ctx.terminateAndAssertInexistanceOfTimeout() }
+    _prerequisites = None
+    super.afterAll()
+  }
 
   /**
     * Returns a class loader that can be used to access resources
@@ -40,8 +61,8 @@ abstract class AbstractGobraTests extends AnnotationBasedTestSuite {
     }
   }
 
-  override def buildTestInput(file: Path, prefix: String): DefaultAnnotatedTestInput =
-    GobraAnnotatedTestInput(file, prefix)
+  override def buildTestInput(file: Path, prefix: String): GobraAnnotatedTestInput =
+    GobraAnnotatedTestInput(file, prefix, prerequisites())
 
   /** we override the default annotation parser because `//@` should not be treated as a comment */
   object GobraAnnotatedTestInput extends TestAnnotationParser {
@@ -49,15 +70,42 @@ abstract class AbstractGobraTests extends AnnotationBasedTestSuite {
       * Creates an annotated test input by parsing all annotations in the files
       * that belong to the given test input.
       */
-    def apply(i: TestInput): DefaultAnnotatedTestInput =
-      DefaultAnnotatedTestInput(i.name, i.prefix, i.files, i.tags,
-        parseAnnotations(i.files))
+    def apply(i: TestInput, prerequisites: (Gobra, GobraExecutionContext)): GobraAnnotatedTestInput =
+      GobraAnnotatedTestInput(i.name, i.prefix, i.files, i.tags,
+        parseAnnotations(i.files), prerequisites)
 
-    def apply(file: Path, prefix: String): DefaultAnnotatedTestInput =
-      apply(DefaultTestInput(file, prefix))
+    def apply(file: Path, prefix: String, prerequisites: (Gobra, GobraExecutionContext)): GobraAnnotatedTestInput =
+      apply(DefaultTestInput(file, prefix), prerequisites)
 
     override def isCommentStart(trimmedLine: String): Boolean = {
       trimmedLine.startsWith(commentStart) && !trimmedLine.startsWith(specificationCommentStart)
     }
+  }
+
+  case class GobraAnnotatedTestInput(
+                                      name: String,
+                                      prefix: String,
+                                      files: Seq[Path],
+                                      tags: Seq[Tag],
+                                      annotations: TestAnnotations,
+                                      prerequisites: (Gobra, GobraExecutionContext)
+                                    ) extends AnnotatedTestInput {
+    /**
+      * Create a test input that is specific to the given project.
+      *
+      * It creates an additional tag, filters files according to annotations
+      * and also filters the annotations themselves.
+      */
+    def makeForProject(projectInfo: ProjectInfo): GobraAnnotatedTestInput = {
+      val ignore = (file: Path) => projectInfo.projectNames.exists(annotations.isFileIgnored(file, _))
+      copy(
+        name = s"$name [${projectInfo.fullName}]",
+        files = files.filter(!ignore(_)),
+        tags = projectInfo.projectNames.map(Tag(_)) ++ tags.toList,
+        annotations = annotations.filterByProject(projectInfo),
+        prerequisites = prerequisites)
+    }
+
+    override def copyWithFiles(files: Seq[Path]): AnnotatedTestInput = copy(files = files)
   }
 }

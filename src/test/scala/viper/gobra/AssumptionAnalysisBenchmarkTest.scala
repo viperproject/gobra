5ea5803c9944c6a16360c64a814fabb14fded8f7
Andrea Keusch <andrea.keusch@hotmail.com>
2025-08-04 11:27:38 +0200

    add performance benchmark

---
+++ src/test/scala/viper/gobra/AssumptionAnalysisBenchmarkTest.scala
@@ -0,0 +1,33 @@
+package viper.gobra
+
+import org.rogach.scallop.throwError
+import viper.gobra.frontend.{Config, ScallopGobraConfig}
+import viper.gobra.reporting.NoopReporter
+import viper.gobra.util.Violation
+
+import java.nio.file.Path
+
+class AssumptionAnalysisBenchmarkTest extends OverallBenchmarkTests {
+  val configOptionsPropertyName = "GOBRATESTS_FLAGS"
+
+  // reuse the same frontend for all test files
+  override val gobraFrontend: GobraFrontendForTesting = new AssumptionAnalysisGobraFrontend()
+
+  class AssumptionAnalysisGobraFrontend extends OverallGobraFrontend {
+    override def reset(files: Seq[Path]): Unit =
+      {
+        val configFlags = System.getProperty(configOptionsPropertyName, "").split(",").filter(_.nonEmpty).toSeq
+        config = Some(createConfig(Array("-i", files.toVector.mkString(" ")) ++ configFlags))
+      }
+
+    private def createConfig(args: Array[String]): Config = {
+      // set throwError to true: Scallop will throw an exception instead of terminating the program in case an
+      // exception occurs (e.g. a validation failure)
+      throwError.value = true
+      // Simulate pick of package, Gobra normally does
+      val config = new ScallopGobraConfig(args.toSeq).config
+      Violation.violation(config.isRight, "creating the config has failed")
+      config.toOption.get.copy(reporter = NoopReporter, z3Exe = z3Exe)
+    }
+  }
+}
---
+++ src/test/scala/viper/gobra/assumptionAnalysisBenchmark.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+TARGET=src/test/resources/andrea/performance_benchmark 
+WARMUP=src/test/resources/andrea/warm-up
+REPS=12
+CSV_BASE=src/test/resources/andrea/performance_benchmark/data
+TIMEOUT=180
+
+BASE_FLAGS="--disablePureFunctsTerminationRequirement"
+ANALYSIS_FLAGS="--disablePureFunctsTerminationRequirement,--enableAssumptionAnalysis,--disableInfeasibilityChecks"
+ANALYSIS_WITH_INFEAS_FLAGS="--disablePureFunctsTerminationRequirement,--enableAssumptionAnalysis"
+
+function run(){
+  SBT_ARGS="
+    test:runMain 
+    -DGOBRATESTS_TARGET=$TARGET
+    -DGOBRATESTS_WARMUP=$WARUMUP 
+    -DGOBRATESTS_REPETITIONS=$REPS
+    -DGOBRATESTS_CSV=${CSV_BASE}_$2.csv
+    -DGOBRATESTS_TIMEOUT=$TIMEOUT
+    -DGOBRATESTS_FLAGS="$1" 
+    org.scalatest.tools.Runner 
+    -o -s 
+    viper.gobra.AssumptionAnalysisBenchmarkTest
+  "
+  echo "Executing $NAME\n$SBT_ARGS"
+  sbt "$SBT_ARGS"
+}
+
+run $BASE_FLAGS "baseline"
+run $ANALYSIS_FLAGS "analysis"
+run $ANALYSIS_WITH_INFEAS_FLAGS "analysis_with_infeas_checks"
+
+

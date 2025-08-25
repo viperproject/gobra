6ef2c572a50a66ba8af2156006ed9147b97820b1
João Pereira <joaopereira.19@gmail.com>
2025-08-25 21:58:23 +0200

    merge pr 961 here

--- src/main/scala/viper/gobra/translator/encodings/StringEncoding.scala
+++ src/main/scala/viper/gobra/translator/encodings/StringEncoding.scala
@@ -412,7 +412,8 @@ class StringEncoding extends LeafTypeEncoding {
         terminationMeasures = Vector(in.NonItfMethodWildcardMeasure(None)(info)),
         backendAnnotations = Vector.empty,
         body = None,
-        isOpaque = false
+        isOpaque = false,
+        isHyper = false
       )(info)
       val translatedFunc = ctx.function(func)
       translatedFunc.res

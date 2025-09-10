798541b15da656fc82239d8128fad1b60ecefba3
Andrea Keusch <andrea.keusch@hotmail.com>
2025-09-10 17:19:07 +0200

    add scion benchmarks

---
+++ src/test/resources/andrea/scion-benchmarks/plotter.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def get_pretty_name(name: str):
+  return name.replace("_", " ").replace("data", "").replace(".csv", "")
+
+def import_result(file: str) -> dict:
+  with open(file, mode="r") as f:
+    contents = f.readlines()
+  results = {}
+  for line in contents[1:]:
+      parts = line.split(',')
+      test_name = parts[0].strip()
+      runtimes = [int(x.strip()) for x in parts[1:]]
+      results[test_name] = runtimes
+  return results
+
+def plot_result_with_err(ax, result_file, x, width):
+  test_results = import_result(result_file)
+  values = []
+  stdDevs = []
+  for name in test_results.keys():
+    values.append(np.sum(test_results[name]))
+    stdDevs.append(np.std(test_results[name]))
+
+  ax.bar(x, values, width, yerr=stdDevs, label=get_pretty_name(result_file))
+
+
+def plot_absolute(result_files: list[str]):
+  width = 0.8/len(result_files)
+  print(width)
+  fig, ax = plt.subplots(figsize=(12, 6))
+
+  # get test names from first result file
+  res = import_result(result_files[0])
+  names = res.keys()
+  x = np.arange(len(names))
+  
+  # plot results from each file
+  for i, file in enumerate(result_files):
+    plot_result_with_err(ax, file, x + (-0.4 + (i+0.5)*width), width)
+
+  # finish plot
+  ax.set_ylabel("Runtime [s]")
+  ax.set_title("Verification Runtime for the VerifiedSCION addr package")
+  ax.set_xticks(x)
+  ax.set_xticklabels(names, rotation=90)
+  # ax.legend()
+
+  plt.tight_layout()
+  plt.savefig("addrPkgRuntimes.png")
+
+input_files = input("Input file: ").split(",")
+
+plot_absolute(input_files)
---
+++ src/test/resources/andrea/scion-benchmarks/scion-benchmark.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+BASE_FLAGS=
+ANALYSIS_FLAGS="--enableAssumptionAnalysis --disableInfeasibilityChecks"
+ANALYSIS_WITH_INFEAS_FLAGS="--enableAssumptionAnalysis"
+PATH_TO_VERIFIED_SCION=C:/Users/Andrea/Documents/ETH/FS25/MasterThesis/VerifiedSCION
+
+RESULT_FILE=scion_benchmark_$(date '+%Y_%m_%d_%H_%M_%S').csv
+touch $RESULT_FILE
+echo "test name,runtimes [s]" > $RESULT_FILE
+
+function run(){
+  ARGS="-Xss1g -Xmx4g 
+  -jar $PATH_TO_VERIFIED_SCION/gobra/gobra.jar 
+  -p $PATH_TO_VERIFIED_SCION/pkg/addr 
+  --norespectFunctionPrePermAmounts 
+  --backend SILICON 
+  --chop 1 
+  -I $PATH_TO_VERIFIED_SCION $PATH_TO_VERIFIED_SCION/verification/dependencies 
+  --onlyFilesWithHeader 
+  -m github.com/scionproto/scion 
+  --assumeInjectivityOnInhale --checkConsistency 
+  --mceMode=od --experimentalFriendClauses 
+  --moreJoins off 
+  -g /tmp/
+  $2
+  $3
+  " 
+  echo "Executing $1"
+  echo "$ARGS"
+  total_duration=0
+  NUM_RUNS=3
+  runtimes=""
+  for i in $(seq 1 $NUM_RUNS); do
+    echo $i
+    start=$(date +%s)
+    java $ARGS
+    end=$(date +%s)
+    duration=$((end-start))
+    total_duration=$(((total_duration+duration)));
+    runtimes+=",$duration"
+  done
+  res=$((total_duration/NUM_RUNS))
+  echo "Finished $1: mean runtime [s] ${res}s"
+  echo "$1$runtimes" >> $RESULT_FILE
+}
+
+run "baseline" $BASE_FLAGS 
+run "analysis_sound" $ANALYSIS_FLAGS 
+run "analysis_with_infeas_checks" $ANALYSIS_WITH_INFEAS_FLAGS 

74685613388843620ff185671be15cfd31000a91
Andrea Keusch <andrea.keusch@hotmail.com>
2025-09-10 17:09:00 +0200

    add benchmark results, nicer plots

--- src/test/resources/andrea/performance_benchmark/plotter.py
+++ src/test/resources/andrea/performance_benchmark/plotter.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+colors = ["#4e6ea7", "#6aa84f", "#cc4125", "#c27ba0"]
+
 def get_pretty_name(name: str):
   return name.replace("_", " ").replace("data", "").replace(".csv", "")
 
@@ -12,28 +14,30 @@ def import_result(file: str):
   for line in contents[1:]:
       parts = line.split(',')
       test_name = parts[0].strip()
-      runtimes = [int(x.strip()) for x in parts[1:]]
+      runtimes = [float(x.strip())/1000.0 for x in parts[1:]]
       results[test_name] = runtimes
   return header[1:], results
 
-def plot_result_with_err(ax, test_names, result_file, idx, x, width):
+def plot_result_with_err(ax, test_names, result_file, idx, x, width, color):
   h, test_results = import_result(result_file)
   stdDevIdx = h.index("StdDev [ms]")
   values = []
   stdDevs = []
+  print(get_pretty_name(result_file))
   for name in test_names:
+    print(f"{name.replace("_", "\_").replace(".gobra", "")} &  {test_results[name][idx]:.2f} \\\\\\hline")
     values.append(test_results[name][idx])
     stdDevs.append(test_results[name][stdDevIdx])
 
-  ax.bar(x, values, width, yerr=stdDevs, label=get_pretty_name(result_file))
+  ax.bar(x, values, width, yerr=stdDevs, capsize=3, label=get_pretty_name(result_file), color=color)
 
-def plot_result(ax, test_names, result_file, idx, x, width):
+def plot_result(ax, test_names, result_file, idx, x, width, color):
   h, test_results = import_result(result_file)
   values = []
   for name in test_names:
     values.append(test_results[name][idx])
 
-  ax.bar(x, values, width, label=get_pretty_name(result_file))
+  ax.bar(x, values, width, label=get_pretty_name(result_file), color=color)
 
 
 def plot_absolute(result_files: list[str], idx: int, withErr=False):
@@ -49,15 +53,15 @@ def plot_absolute(result_files: list[str], idx: int, withErr=False):
   # plot results from each file
   for i, file in enumerate(result_files):
     if withErr:
-      plot_result_with_err(ax, names, file, idx, x + (-0.4 + (i+0.5)*width), width)
+      plot_result_with_err(ax, names, file, idx, x + (-0.4 + (i+0.5)*width), width, color=colors[i])
     else:
-      plot_result(ax, names, file, idx, x + (-0.4 + (i+0.5)*width), width)
+      plot_result(ax, names, file, idx, x + (-0.4 + (i+0.5)*width), width, color=colors[i])
 
   # finish plot
-  ax.set_ylabel(h[idx])
-  ax.set_title(h[idx])
+  ax.set_ylabel(h[idx].replace("Mean [ms]", "Mean Verification Time [s]").replace("[ms]", "[s]"))
+  # ax.set_title("Verification Times")
   ax.set_xticks(x)
-  ax.set_xticklabels(names, rotation=90)
+  ax.set_xticklabels([n.replace(".gobra", "") for n in names])
   ax.legend()
 
   plt.tight_layout()
@@ -66,5 +70,6 @@ def plot_absolute(result_files: list[str], idx: int, withErr=False):
 input_files = input("Input file: ").split(",")
 
 h, res = import_result(input_files[0])
-for idx, _ in enumerate(h):
-  plot_absolute(input_files, idx, True)
+plot_absolute(input_files, 1, True)
+# for idx, _ in enumerate(h):
+  # plot_absolute(input_files, idx, True)
--- src/test/resources/andrea/query_performance_results/plotter.py
+++ src/test/resources/andrea/query_performance_results/plotter.py
@@ -3,17 +3,20 @@ import numpy as np
 import os
 
 csv_files = [f for f in os.listdir(os.getcwd()) if f.endswith(".csv")]
+colors = ["#4e6ea7", "#6aa84f", "#cc4125", "#c27ba0"]
+
 
 def get_pretty_name(name: str):
-  return name.replace(".csv", ".gobra")
+  return name.replace(".csv", "").replace("scion_addr", "VerifiedSCION addr package")
 
 def import_result(file: str):
   with open(file, mode="r") as f:
     contents = f.readlines()
   results = []
+  print("\multicolumn{3}{|l|}{" + get_pretty_name(file).replace("_", "\_") + "}&&\\\\")
   for line in contents[1:]:
       parts = line.split(',')
-      line = int(parts[0].strip())
+      line = parts[0].strip()
       lowLevelDeps = int(parts[1].strip())
       deps = int(parts[2].strip())
       runtimes = [float(x.strip()) for x in parts[3:]]
@@ -22,34 +25,41 @@ def import_result(file: str):
       curr_dict['lowLevelDeps'] = lowLevelDeps
       curr_dict['deps'] = deps
       curr_dict['runtimes'] = runtimes
+      print(f"{line} & {lowLevelDeps} & {deps} & {np.mean(runtimes):.1f} & {np.std(runtimes):.1f}\\\\")
       results.append(curr_dict)
+  print("\hline\hline")
   return results
 
 
-def plot_result(ax, result_file, x_key, marker):
+def plot_result(ax, result_file, x_key, marker, color):
   test_results = import_result(result_file)
   xs= []
   ys = []
   errs = []
   for rs in test_results:
-    xs.append(rs[x_key])
-    ys.append(np.sum(rs['runtimes']))
-    errs.append(np.std(rs['runtimes']))
+    xs_curr = rs[x_key]
+    xs.append(xs_curr)
+    curr_runtime = np.mean(rs['runtimes'])
+    ys.append(curr_runtime)
+    curr_std = np.std(rs['runtimes'])
+    errs.append(curr_std)
 
-  ax.errorbar(xs, ys, yerr=errs, fmt=marker, linestyle='none', capsize=4, label=get_pretty_name(result_file))
+  ax.errorbar(xs, ys, yerr=errs, fmt=marker, color=color, linestyle='none', capsize=3, label=get_pretty_name(result_file))
 
 
 def plot_absolute(result_files: list[str], x_key: str):
   fig, ax = plt.subplots(figsize=(12, 6))
   markers = ['o', 's', '^', 'D']
 
+  print("\\textbf{queried line} & \\textbf{\\#low-level deps} & \\textbf{\\#Gobra deps} & \\textbf{mean [ms]} & \\textbf{std}\\\\\\hline")
+
   for i, file in enumerate(result_files):
-    plot_result(ax, file, x_key, markers[i%len(markers)])
+    plot_result(ax, file, x_key, markers[i%len(markers)], color=colors[i])
 
   # finish plot
-  ax.set_ylabel("runtime [ms]")
-  ax.set_xlabel("#" + x_key)
-  ax.set_title("Runtimes of Dependency Set Queries")
+  ax.set_ylabel("Query Runtime [ms]")
+  ax.set_xlabel("#dependencies")
+  # ax.set_title("Runtimes of Dependency Set Queries")
   ax.legend()
 
   plt.tight_layout()

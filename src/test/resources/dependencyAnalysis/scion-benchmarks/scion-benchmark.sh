bd2f45d4bf174dc752f82b8a0ae79c07b13e1f1d
Andrea Keusch <andrea.keusch@hotmail.com>
2026-02-02 09:45:02 +0100

    update scion benchmark script

--- src/test/resources/dependencyAnalysis/scion-benchmarks/scion-benchmark.sh
+++ src/test/resources/dependencyAnalysis/scion-benchmarks/scion-benchmark.sh
@@ -5,15 +5,16 @@ ANALYSIS_FLAGS="--enableDependencyAnalysis --disableInfeasibilityChecks"
 ANALYSIS_WITH_INFEAS_FLAGS="--enableDependencyAnalysis"
 PATH_TO_VERIFIED_SCION="C:/Users/andre/dev/viper/VerifiedSCION"
 PATH_TO_GOBRA="C:/Users/andre/dev/viper/gobra"
+SCION_PACKAGE=$1
+RESULT_FILE=$2_$(date '+%Y_%m_%d_%H_%M_%S').csv
 
-RESULT_FILE=scion_benchmark_$(date '+%Y_%m_%d_%H_%M_%S').csv
 touch $RESULT_FILE
 echo "test name,runtimes [s]" > $RESULT_FILE
 
 function run(){
   ARGS="-Xss1g -Xmx4g 
   -jar $PATH_TO_GOBRA/target/scala-2.13/gobra.jar
-  -p $PATH_TO_VERIFIED_SCION/pkg/addr
+  -p $PATH_TO_VERIFIED_SCION/$SCION_PACKAGE
   --norespectFunctionPrePermAmounts 
   --backend SILICON 
   --chop 1 
@@ -30,7 +31,7 @@ function run(){
   echo "Executing $1"
   echo "$ARGS"
   total_duration=0
-  NUM_RUNS=5
+  NUM_RUNS=10
   runtimes=""
   for i in $(seq 1 $NUM_RUNS); do
     echo $i

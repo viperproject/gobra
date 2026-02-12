62d1d39acccc70f06c23e5798ff36e805595f18e
Andrea Keusch <andrea.keusch@hotmail.com>
2026-02-12 15:19:32 +0100

    minor fix

--- src/main/scala/viper/gobra/dependencyAnalysis/GobraAnalysisSourceInfo.scala
+++ src/main/scala/viper/gobra/dependencyAnalysis/GobraAnalysisSourceInfo.scala
@@ -7,7 +7,7 @@ import viper.silver.ast.Position
 case class GobraAnalysisSourceInfo(pNode: PNode, pos: Position) extends AnalysisSourceInfo {
   override def toString: String = getDescription + " (" + super.toString + ")"
 
-  override def getDescription: String = pNode.toString
+  override def getDescription: String = pNode.toString.replaceAll("\n", "\t")
 
   override def getPosition: Position = pos
 }

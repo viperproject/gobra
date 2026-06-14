b8be50447d7cf036c860887e17e0a7c8fa300a2c
João Pereira <joaopereira.19@gmail.com>
2026-06-14 10:42:13 +0000

    Add comment on the call to pure in the encoding

--- src/main/scala/viper/gobra/translator/encodings/typeless/AssertionEncoding.scala
+++ src/main/scala/viper/gobra/translator/encodings/typeless/AssertionEncoding.scala
@@ -70,6 +70,11 @@ class AssertionEncoding extends Encoding {
     case as: in.Asserting =>
       for {
         a <- ctx.assertion(as.assertion)
+        // `pure` folds any auxiliary (pure) code emitted while encoding the body
+        // into the body expression itself, keeping it inside the `vpr.Asserting`
+        // node so that `e2` is sequenced *after* `e1`. In practice, this is
+        // a defensive strategy (mirroring `unfolding`'s encoding) that is
+        // not strictly necessary at the moment.
         e <- pure(ctx.expression(as.in))(ctx)
       } yield withSrc(vpr.Asserting(a, e), as)
 
